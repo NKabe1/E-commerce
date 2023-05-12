0c0ae647f97fd447464c9cde11ee4058a21dd9d2
NKabe1 <niino.abesadze@gmail.com>
2023-05-12 14:14:50 +0400

    fix(modified-auth-routes-structure)

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,30 @@
 import { lazy, Suspense } from "react";
 import { Routes, Route } from "react-router-dom";
 import { MainLayout } from "./layouts/MainLayout";
+import RegisterView from "./views/AuthView/RegisterView/RegisterView";
 
 const HomeView = lazy(() => import("./views/HomeView"));
 const ProductsView = lazy(() => import("./views/ProductsView"));
 const ProductView = lazy(() => import("./views/ProductView"));
-const AuthView = lazy(() => import("./views/AuthView"));
+const LoginView = lazy(() => import("./views/AuthView/LoginView"));
+const TegisterView = lazy(() => import("./views/AuthView/RegisterView"));
 
 function App() {
-  
   return (
-    <div >
+    <div>
       <Suspense>
-      <Routes>
-        <Route element={<MainLayout />}>
-          <Route path="/" element={<HomeView />} />
-          <Route path="/products" element={<ProductsView />} />
-          <Route path="/products/:id" element={<ProductView />} />
-          <Route path="/auth/*" element={<AuthView/>}/>
-        </Route>
-      </Routes>
-    </Suspense>
+        <Routes>
+          <Route element={<MainLayout />}>
+            <Route path="/" element={<HomeView />} />
+            <Route path="/products" element={<ProductsView />} />
+            <Route path="/products/:id" element={<ProductView />} />
+            <Route path="/auth-login" element={<LoginView />} />
+            <Route path="/auth-register" element={<RegisterView />} />
+          </Route>
+        </Routes>
+      </Suspense>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/views/AuthView/AuthView.tsx
+++
@@ -1,5 +0,0 @@
-export default function AuthView () {
-    return (
-        <div>This is Auth view</div>
-    )
-}
